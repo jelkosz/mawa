1332c5e52edc58952722a5ed153e916df9162e60
Tomas Jelinek <jelkosz@gmail.com>
2025-07-12 08:09:11 +0200

    Changed the cache to use diskcache instead of memory

--- src/mawa/cache.py
+++ src/mawa/cache.py
@@ -1,20 +1,35 @@
-from cachetools import LRUCache
+from diskcache import Cache
 import hashlib
 
-cache = LRUCache(maxsize=100)
+# Initialize the cache. The 'cache_dir' is where the files will be stored.
+cache = Cache('/tmp/mawacache')
 
 def store_to_cache(key, value):
-    cache[key_to_hash(key)] = value
+    """
+    Stores a value in the file-based cache.
+    """
+    cache.set(key_to_hash(key), value)
 
 def is_cached(key):
+    """
+    Checks if a key is present in the file-based cache.
+    """
     return key_to_hash(key) in cache
 
 def get_from_cache(key):
-    return cache[key_to_hash(key)]
+    """
+    Retrieves a value from the file-based cache.
+    """
+    return cache.get(key_to_hash(key))
 
 def clear_from_cache(key):
-    if key in cache:
-        del cache[key]
+    """
+    Removes a key-value pair from the file-based cache.
+    """
+    cache.delete(key_to_hash(key))
 
 def key_to_hash(key):
+    """
+    Hashes the key to create a unique filename.
+    """
     return hashlib.sha256(key.encode('utf-8')).hexdigest()
